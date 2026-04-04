8ca2dfa15acb136a1f3cfe7636fa5893a19c0ca9
FlorianMiceli <florian.miceli@universite-paris-saclay.fr>
2026-04-04 23:59:40 +0200

    add vercel analytics

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,8 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { Analytics } from "@vercel/analytics/vue"
+</script>
 <template>
+    <Analytics />
     <Header />
     <Rays />
     <router-view v-slot="{ Component }">
